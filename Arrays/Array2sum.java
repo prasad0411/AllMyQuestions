a93a60b8dffbf5d33db282a085426c806d1b402f
Prasad Kanade <65277692+prasad0411@users.noreply.github.com>
2023-06-28 23:28:38 +0530

    Added comments

--- Arrays/Array2sum.java
+++ Arrays/Array2sum.java
@@ -1,4 +1,3 @@
-
 public class Array2sum {
     public static void main(String[] args) {
         int arr[] = new int[] { 1, 2, 33, 4 };
@@ -6,20 +5,27 @@ public class Array2sum {
         twoSum(arr, target);
     }
 
+    // Method to find two numbers in the array that add up to the target
     static int[] twoSum(int arr[], int target) {
-
+        // Check if the array is empty
         if (arr.length < 0) {
             return null;
         }
+
         int i, j;
         j = 0;
+        // Iterate over each element in the array
         for (i = 0; i < arr.length; i++) {
+            // Check the sum of the current element with all subsequent elements
             for (j = i + 1; j < arr.length; j++) {
+                // If the sum of two elements is equal to the target and the elements are not the same,
+                // return the indices of the two elements as an array
                 if (arr[i] + arr[j] == target && i != j) {
                     return new int[] { i, j };
                 }
             }
         }
+        // If no such pair is found, return null
         return null;
     }
 }
