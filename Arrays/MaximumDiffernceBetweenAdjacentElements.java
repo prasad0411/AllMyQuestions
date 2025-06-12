783df21f2356b6b2bed2fdc4b1f59e87f4c30f31
prasad0411 <prasadckanade@gmail.com>
2025-06-12 18:44:16 +0530

    note: optimized the code, MaximumDiffernceBetweenAdjacentElements

--- Arrays/MaximumDiffernceBetweenAdjacentElements.java
+++ Arrays/MaximumDiffernceBetweenAdjacentElements.java
@@ -1,17 +1,32 @@
 package ARRAYS;
 
+// COMPUTE THE MAX ABS DIFFERENCE BETWEEN NEIGHBOURING ELEMENTS IN A CIRCULAR ARRAY
+
+// Input: nums = [-5,-10,-5]
+// Output: 5
+
+// Input: nums = [1,2,4]
+// Output: 3
+
 public class MaximumDiffernceBetweenAdjacentElements {
     public static void main(String[] args) {
         int nums[] = new int[] { -5, -10, -5 };
+        if (nums == null || nums.length < 2) {
+            System.out.println("Invalid input array");
+        }
+
         System.out.println(maxAdjacentDistance(nums));
     }
+
+    // TC: O(n) Traverse the array once
+    // SC: O(1) No new data structure
 
     public static int maxAdjacentDistance(int[] nums) {
         int max = Math.abs(nums[0] - nums[nums.length - 1]);
 
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length - 1; i++)
             max = Math.max(max, Math.abs(nums[i] - nums[i + 1]));
-        }
+
         return max;
     }
 }
