a3016db369c89e9e15f4fa55e3a3e4abc5c6c7f9
Prasad Kanade <prasadckanade@gmail.com>
2025-11-09 21:09:27 -0500

    Optimised MergeSortedArrays Q.

--- ARRAYS/MergeSortedArrays.java
+++ ARRAYS/MergeSortedArrays.java
@@ -15,71 +15,68 @@ public class MergeSortedArrays {
     public static void main(String[] args) {
 
         int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
-        int[] nums2 = new int[] { 2, 5, 6 };
+        int[] nums2 = new int[] { 4, 5, 6 };
         int m = 3, n = 3;
 
-        // int ansArray[] = bruteForceApproach(nums1, m, nums2, n);
-        // System.out.print(Arrays.toString(ansArray));
+        System.out.println("Original array 1 is :" + Arrays.toString(nums1) + ", with non zero values: " + m);
+        System.out.println("Original array 2 is :" + Arrays.toString(nums2) + ", with non zero values: " + n);
 
-        // betterThanBruteForceApproach(nums1, m, nums2, n);
-        optimalApproach(nums1, m, nums2, n);
-        System.out.print(Arrays.toString(nums1));
+        // System.out.print(Arrays.toString(bruteForceApproach(nums1, m, nums2, n)));
+        System.out.print(Arrays.toString(optimalApproach(nums1, m, nums2, n)));
     }
+
+    // APPROACH: Use 2 pointers, one for array 1, another for array 2, and compare
+    // each' elements. Based on smaller value, put them in the result array (newly
+    // created)
 
     // Time Complexity: O(m+n).
     // Each element from nums1 and nums2 is processed once
     // Space Complexity: O(m+n).
     // A new array of size (m+n) is created.
     public static int[] bruteForceApproach(int[] nums1, int m, int[] nums2, int n) {
-        System.out.print("Brute force approach => ");
+        System.out.println("Brute Force => ");
+        if (nums1 == null || nums2 == null)
+            throw new IllegalArgumentException("Arrays cannot be null.");
+
+        int newArray[] = new int[m + n];
         int i = 0, j = 0, k = 0;
-        int ansArray[] = new int[nums1.length];
         while (i < m && j < n) {
-            if (nums1[i] < nums2[j])
-                ansArray[k++] = nums1[i++];
-            else
-                ansArray[k++] = nums2[j++];
+            if (nums1[i] < nums2[j]) {
+                newArray[k++] = nums1[i++];
+            } else {
+                newArray[k++] = nums2[j++];
+            }
         }
+
         while (i < m) {
-            ansArray[k++] = nums1[i++];
+            newArray[k++] = nums1[i++];
         }
         while (j < n) {
-            ansArray[k++] = nums2[j++];
+            newArray[k++] = nums2[j++];
         }
-        return ansArray;
+
+        return newArray;
     }
 
-    // TC: O((m+n) log(m+n)).
-    // Add n elements from nums2 to nums1: O(n). Sort nums1: O(m+n log m+n)
-    // SC: O(1). No extra space for any data structure.
-    // Modified nums1 in place. So need not return that as well
-    public static void betterThanBruteForceApproach(int[] nums1, int m, int[] nums2, int n) {
-        System.out.print("Better than brute force approach => ");
-
-        int j = 0;
-        for (int i = m; i < m + n; i++) {
-            nums1[i] = nums2[j++];
-        }
-        Arrays.sort(nums1);
-    }
-
+    // Approach: Use the 0 values of array 1 to populate the biggest elements first.
+    // and then we populate the smallest elements.
     // TC: O(m+n).
     // Add m+ n elements from nums1 and nums2 to result array
     // SC: O(1). No new data structure used. Modified nums1 in place
-    public static void optimalApproach(int[] nums1, int m, int[] nums2, int n) {
+    public static int[] optimalApproach(int[] nums1, int m, int[] nums2, int n) {
         System.out.print("Optimal Approach => ");
-
-        int i = m - 1;
-        int j = n - 1;
-        int newIndexes = m + n - 1;
+        int k = nums1.length - 1, i = m - 1, j = n - 1;
         while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j])
-                nums1[newIndexes--] = nums1[i--];
-            else
-                nums1[newIndexes--] = nums2[j--];
+            if (nums2[j] > nums1[i]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
         }
-        while (j >= 0)
-            nums1[newIndexes--] = nums2[j--];
 
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+        return nums1;
     }
 }
