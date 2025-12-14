8475aebfb3d4a06e08c152cc193028927b596b7c
Prasad Kanade <prasadckanade@gmail.com>
2025-12-14 12:49:09 -0500

    added comments about the TC and SC for this question.

--- searching/SearchInsertPosition.java
+++ searching/SearchInsertPosition.java
@@ -2,22 +2,35 @@ package searching;
 
 import java.util.Arrays;
 
-// AN ARRAY IS GIVEN. 
+// FIND THE INDEX OF THE TARGET ELEMENT FROM THE SORTED ARRAY.
+// IF THAT ELEMENT IS NOT PRESENT, RETURN THE INDEX WHERE IT WOULD BE PLACED IN THE ARRAY.
 
-// Input: [[1,2,3],[4,5,6],[7,8,9]]  
-// Output: [1,2,3,6,9,8,7,4,5]
+// Input: [1,3,5,6], 5
+// Output: 2
 
-// Input: [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
-// Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+// Input: [1,3,5,6], 2
+// Output: 1
+
+// Input: [1,3,5,6], 7
+// Output: 4
+
 public class SearchInsertPosition {
     public static void main(String args[]) {
         int[] originalArray = { 1, 3, 5, 6 };
-        int target = 9;
+        int target = 4;
+
         System.out.println("Original array is: " + Arrays.toString(originalArray));
-        System.out.println("Insert position is: " + searchInsert(originalArray, target));
+        System.out.println("Insert position is: " + optimalApproach(originalArray, target));
     }
 
-    public static int searchInsert(int[] nums, int target) {
+    // APPROACH: Use Binary search to make use of sorted array. Keep comparing
+    // target element with the middle element. When the loop exists, the
+    // leftPointer is the index to insert the target element.
+
+    // TC: O(log (n)).
+    // Constanly dividing the array into 2 halves
+    // O(1)
+    public static int optimalApproach(int[] nums, int target) {
         // BASE CASE
         if (nums == null || nums.length == 0) {
             throw new IllegalArgumentException("Array is of invalid size.");
@@ -35,6 +48,7 @@ public class SearchInsertPosition {
             } else
                 rightPointer = midPointer - 1;
         }
+
         return leftPointer;
     }
 }
