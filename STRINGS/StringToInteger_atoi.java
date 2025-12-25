c28a07907e5eb5938dc9f94547b1b957347054af
Prasad Kanade <prasadckanade@gmail.com>
2025-12-25 12:57:17 -0500

    Wrote the optimised code for String To Integer (ATOI)

--- STRINGS/StringToInteger_atoi.java
+++ STRINGS/StringToInteger_atoi.java
@@ -1,56 +1,41 @@
 package strings;
 
+// RETURN THE INTEGER EQUIVALENT OF THE STRING VALUE
 public class StringToInteger_atoi {
     public static void main(String[] args) {
-        String s = "20000000000000000000";
+        String s = "   -042";
         System.out.println("Original string is: " + s);
         System.out.println("Integer form of the string is: " + myAtoi(s));
     }
 
     public static int myAtoi(String s) {
-        int finalValue = 0;
         s = s.trim();
 
         if (s.isEmpty()) {
             return 0;
         }
 
-        int traversal = 0;
+        int i = 0;
+        int sign = 1;
+        long finalValue = 0;
 
-        char[] originalChractacters = s.toCharArray();
-        StringBuilder finalString = new StringBuilder();
-        boolean isPositive = true;
-
-        if (originalChractacters[0] == '-') {
-            isPositive = false;
-            traversal = 1;
-        } else if (originalChractacters[0] == '+') {
-            traversal = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
         }
 
-        for (int i = traversal; i < s.length(); i++) {
-            if (!Character.isDigit(originalChractacters[i])) {
-                break;
-            }
-            finalString.append(originalChractacters[i]);
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            finalValue = finalValue * 10 + s.charAt(i) - '0';
+
+            if (sign * finalValue > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
+            if (sign * finalValue < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+
+            i++;
         }
 
-        if (finalString.isEmpty())
-            return 0;
-
-        long temp = Long.parseLong(finalString.toString());
-
-        if (!isPositive)
-            temp *= -1;
-
-        if (temp > Integer.MAX_VALUE) {
-            finalValue = Integer.MAX_VALUE;
-        } else if (temp < Integer.MIN_VALUE) {
-            finalValue = Integer.MIN_VALUE;
-        } else {
-            finalValue = (int) temp;
-        }
-
-        return finalValue;
+        return (int) (sign * finalValue);
     }
 }
