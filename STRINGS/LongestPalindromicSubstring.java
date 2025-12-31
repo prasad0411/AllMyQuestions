6d5ad4da19d59a34d1ef1aee4d75483eeb2e7a84
Prasad Kanade <prasadckanade@gmail.com>
2025-12-31 00:52:56 -0500

    Wrote the brute force approach for Longest Palindromic Substring. LC:5

--- STRINGS/LongestPalindromicSubstring.java
+++ STRINGS/LongestPalindromicSubstring.java
@@ -1,11 +1,28 @@
 package strings;
 
+// A STRING IS GIVEN. RETURN THE LONGEST PALINDROMIC SUBSTRING. IF THERE ARE MANY SUBSTRINGS, RETURN ANY
+
+// Input: babad
+// Output: bab
+// Output: aba
+
+// Input: cbbd
+// Output: bb
+
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-        String originalString = "babad";
+        String originalString = "cbbd";
         System.out.println("Original string is: " + originalString);
-        System.out.println("Palindrome string is: " + longestPalindrome(originalString));
+        System.out.println("Longest Palindrome SubString is: " + longestPalindrome(originalString));
     }
+
+    // APPROACH: Use 2 loops to traverse the entire string. And use 2 pointers to
+    // traverse the string from left and right side.
+    // If current substring is palindrome, then compare it against the longest
+    // palindromic substring achieved so far and return that
+
+    // TC: O(n * n * n). Two loops O(n * n) * isPalindrome check O(n)
+    // SC: O(1) auxiliary space (output string created is not counted)
 
     public static String longestPalindrome(String s) {
         System.out.println("Brute Force Approach => ");
@@ -13,28 +30,34 @@ public class LongestPalindromicSubstring {
         if (s == null || s.isEmpty())
             throw new IllegalArgumentException("String is empty.");
 
-        StringBuilder palindromeString = new StringBuilder();
-        String maxString = Character.toString(s.charAt(0));
+        String maxString = "";
 
-        for (int i = 0; i < s.length();) {
+        for (int i = 0; i < s.length(); i++) {
             for (int j = i; j < s.length(); j++) {
                 int leftPointer = i;
                 int rightPointer = j;
-                while (leftPointer < rightPointer) {
-                    if (s.charAt(i) != s.charAt(j)) {
-                        break;
-                    } else {
-                        palindromeString.append(s.charAt(i));
-                        i++;
-                        j--;
-                    }
-                }
 
-                maxString = maxString.length() > palindromeString.length() ? maxString : palindromeString.toString();
+                if (isPalindrome(leftPointer, rightPointer, s)) {
+                    String currentSubstring = s.substring(i, j + 1);
+                    maxString = maxString.length() > currentSubstring.length() ? maxString : currentSubstring;
+                }
             }
         }
 
-        // babad
         return maxString;
+    }
+
+    // Checks if current string is palindrome or no
+    public static boolean isPalindrome(int leftPointer, int rightPointer, String s) {
+        while (leftPointer < rightPointer) {
+            if (s.charAt(leftPointer) != s.charAt(rightPointer)) {
+                return false;
+            } else {
+                leftPointer++;
+                rightPointer--;
+            }
+        }
+
+        return true;
     }
 }
