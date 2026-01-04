b116f299c55344d7deb545957ed3972ac52ca52e
Prasad Kanade <prasadckanade@gmail.com>
2026-01-03 22:37:23 -0500

    Wrote the brute force approach for reversing the LL in iterative way.

--- linkedList/ReverseLinkedList.java
+++ linkedList/ReverseLinkedList.java
@@ -1,5 +1,13 @@
 package linkedList;
 
+// REVERSE A LL
+
+// Input: [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Input: [2,3,4]
+// Output: [4,3,2]
+
 public class ReverseLinkedList {
     public static void main(String[] args) {
         ListNode headNode = new ListNode(1);
@@ -7,6 +15,7 @@ public class ReverseLinkedList {
         ListNode thirdNode = new ListNode(3);
         ListNode fourthNode = new ListNode(4);
         ListNode fifthNode = new ListNode(5);
+
         headNode.next = secondNode;
         secondNode.next = thirdNode;
         thirdNode.next = fourthNode;
@@ -23,17 +32,49 @@ public class ReverseLinkedList {
         }
 
         ListNode reversedLLNode = new ListNode();
-        System.out.println("\nReversed LL is: ");
-        reversedLLNode = reverseList(headNode);
+        System.out.println("\n\nReversed LL is: ");
+        reversedLLNode = bruteForceApproach(headNode);
 
         while (reversedLLNode != null) {
             System.out.print(reversedLLNode.val + " -> ");
             reversedLLNode = reversedLLNode.next;
         }
-
     }
 
-    public static ListNode reverseList(ListNode headNode) {
+    // APPROACH: Traverse the original LL, preserve values of all nodes in an array.
+    // Traverse this array from the last index and over write all the values of the
+    // original LL.
+
+    // TC: O(n) to calculate size of LL + O(n) to populate the array from LL + O(n)
+    // to overwrite the LL
+    // SC: O(n) Creating an array, to store the LL values of each node temporarily.
+    public static ListNode bruteForceApproach(ListNode headNode) {
+        System.out.println("Brute Force Approach => ");
+
+        ListNode tempNode = new ListNode();
+        tempNode = headNode;
+
+        int sizeLL = 0;
+        while (tempNode != null) {
+            sizeLL++;
+            tempNode = tempNode.next;
+        }
+
+        tempNode = headNode;
+
+        int[] arrayOfLL = new int[sizeLL];
+        for (int i = 0; i < sizeLL; i++) {
+            arrayOfLL[i] = tempNode.val;
+            tempNode = tempNode.next;
+        }
+
+        ListNode reverseLLPointer = new ListNode();
+        reverseLLPointer = headNode;
+        for (int i = sizeLL - 1; i >= 0; i--) {
+            reverseLLPointer.val = arrayOfLL[i];
+            reverseLLPointer = reverseLLPointer.next;
+        }
+
         return headNode;
     }
 
