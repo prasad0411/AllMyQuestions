ba0352942c157197dae700102b5f5860a2965be0
Prasad Kanade <prasadckanade@gmail.com>
2026-01-16 23:42:06 -0500

    Wrote the brute force approach for LL Cycle 2

--- linkedList/LinkedListCycleII.java
+++ linkedList/LinkedListCycleII.java
@@ -2,6 +2,15 @@ package linkedList;
 
 import java.util.HashMap;
 
+// DETECT IF A CYCLE EXISTS IN THE LL, IF YES, THEN FIND THE POSITION OF THAT NODE
+// IF CYCLE DOES NOT EXIST, THEN RETURN -1
+
+// Input: [1,2,0,4,2]
+// 1
+
+// Input: [2,3,4]
+// -1
+
 public class LinkedListCycleII {
     public static void main(String[] args) {
         ListNode headNode = new ListNode(3);
@@ -13,40 +22,48 @@ public class LinkedListCycleII {
         headNode.next = secondNode;
         secondNode.next = thirdNode;
         thirdNode.next = fourthNode;
-        // fourthNode.next = fifthNode;
+        fourthNode.next = secondNode;
 
         System.out.print("Original LL is: ");
 
         ListNode tempNode = headNode;
-        while (tempNode != null) {
-            System.out.print(tempNode.val);
-            if (tempNode.next != null) {
-                System.out.print(" -> ");
-            }
-            tempNode = tempNode.next;
-        }
+        // while (tempNode != null) {
+        // System.out.print(tempNode.val);
+        // if (tempNode.next != null) {
+        // System.out.print(" -> ");
+        // }
+        // tempNode = tempNode.next;
+        // }
 
         System.out.print("\n\nDoes LL contain a cycle?");
         ListNode node = bruteForceApproach(headNode);
-        System.out.println(node.val);
+        if (node != null)
+            System.out.println(node.val);
+        else
+            System.out.println("LL has no cycle.");
     }
 
-    static HashMap<ListNode, Integer> mappedNodes = new HashMap<>();
+    // APPROACH: Use a fast (moving 2x of slowP), and a slow pointer, if fastP ever
+    // becomes equal to slowP, then a cycle exists
 
+    // TC: O(n) - Traversing the LL once
+    // SC: O(1) - No new data structure. Only creating 2 pointers.
     public static ListNode bruteForceApproach(ListNode headNode) {
         if (headNode == null) {
             return null;
         }
-
-        ListNode tempNode = headNode;
         int i = 0;
 
-        while (tempNode != null) {
-            mappedNodes.put(tempNode, i++);
-            if (mappedNodes.containsKey(tempNode))
-                return tempNode;
+        HashMap<ListNode, Integer> mappedNodes = new HashMap<>();
 
-            tempNode = tempNode.next;
+        while (headNode != null) {
+
+            if (mappedNodes.containsKey(headNode))
+                return headNode;
+            else
+                mappedNodes.put(headNode, i++);
+
+            headNode = headNode.next;
         }
 
         return null;
