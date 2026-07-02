f946ca4994e9e71494664366be8f541fc2ddf5e9
Prasad Kanade <prasadckanade@gmail.com>
2026-07-02 12:56:11 +0530

    Solved the Brute force approach

--- arrays/SortColors.java
+++ arrays/SortColors.java
@@ -1,9 +1,7 @@
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
-// AN UNSORTED ARRAY IS GIVEN, NEED TO SORT IT IN PLACE FOR THE 3 NUMBERS/ COLORS, 0 , 1, 2
+// AN UNSORTED ARRAY IS GIVEN, NEED TO SORT IT IN PLACE FOR THE 3 NUMBERS, 0, 1, 2, WHICH ARE REPRESENTED AS RED, WHITE & BLUE 
 
 // Input: [2, 0, 2, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 0, 2, 2, 2, 1, 1]  
 // Output: [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
@@ -15,46 +13,38 @@ import java.util.Map;
 // Output: [0,1,2]
 
 public class SortColors {
-
     public static void main(String[] args) {
-        int[] inputArray = new int[] { 2, 0, 1 };
+        int[] inputArray = new int[] { 2, 0, 2, 1, 1, 0 };
         System.out.println("Original array is: " + Arrays.toString(inputArray));
-        System.out.println(Arrays.toString(bruteForce(inputArray)));
+        bruteForce(inputArray);
+        System.out.println("Original array is: " + Arrays.toString(inputArray));
     }
 
-    // APPROACH: Use a hashmap to store count of 0,1,2. Based on these counts or
-    // null values, in the array, put 0,1,2
+    // Approach: Iterate over the original array and count the number of 0s, 1s, &
+    // 2. Based on these values, assign those many positions in the original array.
+    private static void bruteForce(int[] inputArray) {
+        int zeroCounter, oneCounter, twoCounter;
+        zeroCounter = oneCounter = twoCounter = 0;
 
-    // TC: O(n). Traversing through the whole array and putting counts of 0,1,2 into
-    // the Map
-    // SC: O(n). Creating hashMap
-    private static int[] bruteForce(int[] inputArray) {
-        // BASE CASE
-        if (inputArray == null || inputArray.length == 0) {
-            throw new IllegalArgumentException("Array is null or has an invalid size.");
-        }
-        System.out.println("Brute force approach: =>");
-
-        Map<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < inputArray.length; i++) {
-            hashMap.put(inputArray[i], hashMap.getOrDefault(inputArray[i], 0) + 1);
+        for (int num : inputArray) {
+            if (num == 0)
+                zeroCounter++;
+            else if (num == 1)
+                oneCounter++;
+            else
+                twoCounter++;
         }
 
-        int counterZero = hashMap.getOrDefault(0, 0);
-        int counterOne = hashMap.getOrDefault(1, 0);
-
-        for (int i = 0; i < counterZero; i++) {
+        for (int i = 0; i < zeroCounter; i++) {
             inputArray[i] = 0;
         }
 
-        for (int i = counterZero; i < counterOne + counterZero; i++) {
+        for (int i = zeroCounter; i < zeroCounter + oneCounter; i++) {
             inputArray[i] = 1;
         }
 
-        for (int i = counterOne + counterZero; i < inputArray.length; i++) {
+        for (int i = zeroCounter + oneCounter; i < inputArray.length; i++) {
             inputArray[i] = 2;
         }
-
-        return inputArray;
     }
 }
