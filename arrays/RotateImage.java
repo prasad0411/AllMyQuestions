4baa7d44e15eaad0b9689f09a447716656e6fe8e
Prasad Kanade <prasadckanade@gmail.com>
2026-07-16 14:38:12 -0400

    Wrote the brute force soln for Rotate Image.

--- arrays/RotateImage.java
+++ arrays/RotateImage.java
@@ -1,15 +1,17 @@
 public class RotateImage {
 
-    // WE HAVE AN N*N MATRIX, AND WE NEED TO ROTATE IT CLOCKWISE BY 90 DEGREES.
+    // LC: 48. Rotate Image.
+    // Problem: We have an N*N matrix, and we need to rotate it clockwise by 90
+    // degrees.
 
-    // INPUT: [[1,2,3],[4,5,6],[7,8,9]]
-    // OUTPUT: [[7,4,1],[8,5,2],[9,6,3]]
+    // Input: [[1,2,3],[4,5,6],[7,8,9]]
+    // Output: [[7,4,1],[8,5,2],[9,6,3]]
 
-    // INPUT: [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-    // OUTPUT: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+    // Input: [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    // Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 
     public static void main(String[] args) {
-        int[][] originalMatrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] originalMatrix = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         int matrixSize = originalMatrix.length;
 
         System.out.println("Original 2D array is: ");
@@ -20,7 +22,7 @@ public class RotateImage {
             System.out.println("");
         }
 
-        optimalApproach(originalMatrix);
+        bruteForce(originalMatrix);
 
         System.out.println("\nRotated 2D array by 90 degrees is: ");
         for (int i = 0; i < matrixSize; i++) {
@@ -31,46 +33,30 @@ public class RotateImage {
         }
     }
 
-    // Approach: We initially transpose the matrix so that we swap the columns with
-    // rows. Use a temp variable to transpose all positions
-    // We start j as i+1 so we can skip swapping diagonals and pairs which were
-    // already swapped
-    // Post transposing, we reverse each row to achieve the 90 degrees condition
+    // Approach: Create a new matrix, and for every row i in the original, place the
+    // elemnent in column (size - i - 1) of rotated matrix;
+    // TC: O(N * N)
+    // SC: O(N * N)
+    private static int[][] bruteForce(int[][] matrix) {
+        int size = matrix.length;
+        int rotatedMatrix[][] = new int[size][size];
 
-    // TC: O(m *n)
-    // Traverse all rows and columns, and use a temp variable to swap all positions
-    // SC: O(1).
-    // No new data structure
-    private static void optimalApproach(int[][] matrix) {
-        if (matrix == null || matrix.length == 0) {
-            throw new IllegalArgumentException("Matrix is null or has incorrect size.");
-        }
+        for (int i = 0; i < size; i++) {
+            int rotatedCol = size - 1 - i;
+            int rotatedRow = 0;
 
-        transpose(matrix);
-        reverse(matrix);
-    }
-
-    private static void transpose(int[][] matrix) {
-        int matrixSize = matrix.length;
-
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = i + 1; j < matrixSize; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+            for (int j = 0; j < size; j++) {
+                rotatedMatrix[rotatedRow][rotatedCol] = matrix[i][j];
+                rotatedRow++;
             }
         }
-    }
 
-    private static void reverse(int[][] matrix) {
-        int matrixSize = matrix.length;
-
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrixSize - 1 - j];
-                matrix[i][matrixSize - 1 - j] = temp;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = rotatedMatrix[i][j];
             }
         }
+
+        return rotatedMatrix;
     }
 }
