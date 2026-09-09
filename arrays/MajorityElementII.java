ebdcdfd2e6cbc7a7643ce72d7469197433c2cbbb
Prasad Kanade <prasadckanade@gmail.com>
2026-09-08 23:21:27 -0400

    Wrote the brute force approach for Majority element 2

--- arrays/MajorityElementII.java
+++ arrays/MajorityElementII.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +20,7 @@ import java.util.Map;
 public class MajorityElementII {
     // Approach: Iterate over the array to store the count of each element in Map
     // Traverse over the Map to check the condition and return those elements.
-    // TC: O(m) + O(n)
+    // TC: O(n)
     // SC: O(n)
     public static List<Integer> majorityElement(int[] nums) {
         List<Integer> majorityElementsList = new ArrayList<>();
@@ -39,8 +40,8 @@ public class MajorityElementII {
     }
 
     public static void main(String[] args) {
-        int array[] = new int[] { 1,2 };
-        System.out.println("Original array is:" + array.toString());
+        int array[] = new int[] { 1, 2 };
+        System.out.println("Original array is:" + Arrays.toString(array));
         System.out.println("Brute Force Approach: " + majorityElement(array));
     }
 }
